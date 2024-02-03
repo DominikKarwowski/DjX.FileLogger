a72aeefbd4eb8645570305796adcaaf4d24c5d02
DjK <karwowski.d@o2.pl>
2024-02-04 00:30:56 +0100

    Add log archiver

---
+++ DjX.FileLogger/Archiver/LogArchivingPolicy.cs
@@ -0,0 +1,11 @@
+﻿namespace DjX.FileLogger.Archiver;
+
+public record LogArchivingPolicy(
+    string LogFileArchiveSubDirectoryPath,
+    ArchivingFrequency ArchivingFrequency,
+    uint LogRetentionNbOfDays = 0);
+
+public enum ArchivingFrequency
+{
+    Daily, Monthly, Yearly
+}
